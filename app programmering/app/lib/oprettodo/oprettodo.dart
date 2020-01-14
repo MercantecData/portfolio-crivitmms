f4a630f2cd91aea97fd6b2fb2b2fbf6aa41fd6cb
crivitmms <morten.mikaelsen.smed@skjern-net.dk>
2020-01-14 13:29:12 +0100

    opretknap

---
+++ app programmering/app/lib/oprettodo/oprettodo.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class OpretTodo extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Text("data");
+  }
+
+}
+
+
+
+
+
+
