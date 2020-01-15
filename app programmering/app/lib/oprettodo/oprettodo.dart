98df1a87969781d5bbe90d516408563884d5dd38
unknown <morten.mikaelsen.smed@skjern-net.dk>
2020-01-15 12:00:51 +0100

    textfelter og knap uden styling eller funktioner

--- app programmering/app/lib/oprettodo/oprettodo.dart
+++ app programmering/app/lib/oprettodo/oprettodo.dart
@@ -1,11 +1,55 @@
 import 'package:flutter/material.dart';
 
-class OpretTodo extends StatelessWidget{
+class OpretTodo extends StatefulWidget{
+
+  @override
+  _OpretTodo createState()=> _OpretTodo();
+}
+
+
+class _OpretTodo extends State{
+
+  final titelControl = TextEditingController();
+  final beskrivelsesControl = TextEditingController();
+
+  @override
+  void dispose(){
+    titelControl.dispose();
+    beskrivelsesControl.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Text("data");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Opret todo"),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: titelControl,
+              decoration: InputDecoration(
+                labelText: 'Titel'
+              ),
+            ),
+            TextField(
+              controller: beskrivelsesControl,
+              maxLines: null,
+              keyboardType: TextInputType.multiline,
+              decoration: InputDecoration(
+                labelText: 'Beskrivelse'
+              ),
+            ),
+            FlatButton(
+              onPressed: null,
+              child: Text("Gem"),
+            )
+          ],
+        ),
+      )
+    );
   }
 
 }
